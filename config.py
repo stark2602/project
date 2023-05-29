272264a7aaa7ba36156330fe4c7222cbe5708c76
Mayank Pratap Singh <98162499+stark2602@users.noreply.github.com>
2023-05-29 19:55:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # default brain settings
 APPLICATION_TITLE = "Theia – NIfTI (nii.gz) 3D Visualizer"
-BRAIN_SMOOTHNESS = 500
-BRAIN_OPACITY = 0.2
-BRAIN_COLORS = [(1.0, 0.9, 0.9)]  # RGB percentages
+LIVER_SMOOTHNESS = 500
+LIVER_OPACITY = 0.2
+LIVER_COLORS = [(1.0, 0.9, 0.9)]  # RGB percentages
 
 # default mask settings
 MASK_SMOOTHNESS = 500
